9a1f81cdbbf6df9a8ba1f0b310ca563688d49ff8
der-b <der-b@der-b.com>
2017-07-02 15:29:11 +0000

    Extend custom_cleanup for object files, log files, executables, trs-files and so on ...

--- custom_clean.sh
+++ custom_clean.sh
@@ -4,3 +4,8 @@
 # script
 find . -name "\.deps" | xargs rm -r
 find . -name Makefile | xargs rm
+rm $(find . -name "*\.o")
+rm $(find . -name "Makefile\.in")
+rm $(find . -name "*\.out")
+rm $(find . -name "*\.log")
+rm $(find . -name "*\.trs")
