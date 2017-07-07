7549e9fac24cbede4b329811eec3f20d481fec21
der-b <der-b@der-b.com>
2017-07-07 00:50:20 +0000

    Optimized config for static code analyzer.

--- test/staticAnalyzing/cppcheck.sh
+++ test/staticAnalyzing/cppcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cppcheck -j 5 --enable=warning,performance,portability --template "{file}({line}):{id}: {message}" --error-exitcode=1 --exitcode-suppressions=cppcheck.suppressions.list.txt -I ../../include ../../
+cppcheck -j 5 --enable=warning,performance,portability --template "{file}({line}):{id}: {message}" --error-exitcode=1 --exitcode-suppressions=cppcheck.suppressions.list.txt -I ../../include ../../src ../../test
