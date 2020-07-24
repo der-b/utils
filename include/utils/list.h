0cebe475311f9c3304b70a3fc8d9b65f8b5876b5
der-b <der-b@der-b.com>
2020-07-24 22:00:44 +0200

    Fixed indention.

--- include/utils/list.h
+++ include/utils/list.h
@@ -43,142 +43,142 @@ typedef struct list_iter LIter;
 typedef size_t LSize;
 
 enum list_type {
-	LIST_TYPE_NONE = 0,
+    LIST_TYPE_NONE = 0,
 
-	/*
-	 * LIST_TYPE_DLINKED -- Double linked list implementation.
-	 *
-	 * Each element is stored together with two pointers. On points to the successor element and
-	 * one points to the predecessor element.
-	 *
-	 * This implementation has the complexity of O(1) for inserting, removing and accessing an element 
-	 * at the head or at the tail. Inserting, removing or accessing a element at a arbitrary position 
-	 * has a complexity of O(n/4). Inserting or removing an element will not cause a reallocation of
-	 * memory and therefore no unecpected runtime peaks whill occure.
-	 *
-	 * Itteration through the array will not profit from caching mechanisms of the CPU, because the 
-	 * elements are stored as a block in the memory. In fact this implementation tends to fraction the
-	 * memory.
-	 */
-	LIST_TYPE_DLINKED,
+    /*
+     * LIST_TYPE_DLINKED -- Double linked list implementation.
+     *
+     * Each element is stored together with two pointers. On points to the successor element and
+     * one points to the predecessor element.
+     *
+     * This implementation has the complexity of O(1) for inserting, removing and accessing an element 
+     * at the head or at the tail. Inserting, removing or accessing a element at a arbitrary position 
+     * has a complexity of O(n/4). Inserting or removing an element will not cause a reallocation of
+     * memory and therefore no unecpected runtime peaks whill occure.
+     *
+     * Itteration through the array will not profit from caching mechanisms of the CPU, because the 
+     * elements are stored as a block in the memory. In fact this implementation tends to fraction the
+     * memory.
+     */
+    LIST_TYPE_DLINKED,
 
-	/*
-	 * LIST_TYPE_STLINKED -- Single linked list (Tail insert, Tail remove)
-	 * TODO: Documentation
-	 */
-	LIST_TYPE_STLINKED,
+    /*
+     * LIST_TYPE_STLINKED -- Single linked list (Tail insert, Tail remove)
+     * TODO: Documentation
+     */
+    LIST_TYPE_STLINKED,
 
-	/*
-	 * LIST_TYPE_ARRAY -- List implementation using an array.
-	 *
-	 *
-	 * Efficient for LIFOs. Usually push_back and pop_back have a computional complexity of O(1).
-	 * In the case, that the array is to small, then a memory reallocation will occour. The
-	 * Reallocation will double or halve the current array size. That means the greater the array
-	 * the more seldom a reallocation will occur. In case of adding an element the reallocation may
-	 * have a superior complexity since the "reaclloc" call may have to copy the array depending of
-	 * the memory fraction.
-	 *
-	 * This implementation is inefficient while inserting or deleting elements which are not at the 
-	 * end of the list. The complexity for removing or inserting a element at the beginning is
-	 * O(n). When the array has to be expanded the complexity may rise due to the copying actions
-	 * of realloc().
-	 *
-	 * Accessing an element has always the complexity O(1) since the position can be calculated.
-	 *
-	 * The implementation is also efficient while itterating through the list since all data is lays
-	 * conteneously in the memory and chaching mechanism of the CPU can be applayed.
-	 *
-	 * In terms of memory usage the implementation is efficient. Only a counter and a pointer to the 
-	 * first element is used by the implementation in theory. Due to the memory  management the actual
-	 * memory useage can be twice as high as really needed.
-	 *
-	 * Please note, that the user of listimplementation have to decide whether he use the list as
-	 * pointer array or he insert whole elements.
-	 */
-	LIST_TYPE_ARRAY
+    /*
+     * LIST_TYPE_ARRAY -- List implementation using an array.
+     *
+     *
+     * Efficient for LIFOs. Usually push_back and pop_back have a computional complexity of O(1).
+     * In the case, that the array is to small, then a memory reallocation will occour. The
+     * Reallocation will double or halve the current array size. That means the greater the array
+     * the more seldom a reallocation will occur. In case of adding an element the reallocation may
+     * have a superior complexity since the "reaclloc" call may have to copy the array depending of
+     * the memory fraction.
+     *
+     * This implementation is inefficient while inserting or deleting elements which are not at the 
+     * end of the list. The complexity for removing or inserting a element at the beginning is
+     * O(n). When the array has to be expanded the complexity may rise due to the copying actions
+     * of realloc().
+     *
+     * Accessing an element has always the complexity O(1) since the position can be calculated.
+     *
+     * The implementation is also efficient while itterating through the list since all data is lays
+     * conteneously in the memory and chaching mechanism of the CPU can be applayed.
+     *
+     * In terms of memory usage the implementation is efficient. Only a counter and a pointer to the 
+     * first element is used by the implementation in theory. Due to the memory  management the actual
+     * memory useage can be twice as high as really needed.
+     *
+     * Please note, that the user of listimplementation have to decide whether he use the list as
+     * pointer array or he insert whole elements.
+     */
+    LIST_TYPE_ARRAY
 };
 
 struct list_iter {
-	enum list_type type;
-	void *data;
-	void *(*next) (LIter * iter);
-	LRet(*lfree) (LIter * iter);
+    enum list_type type;
+    void *data;
+    void *(*next) (LIter * iter);
+    LRet(*lfree) (LIter * iter);
 };
 
 struct list_options {
-	LFlags flags;
-	void *data;
-	LSize size;
+    LFlags flags;
+    void *data;
+    LSize size;
 };
 
 struct list {
-	enum list_type type;
-	LOpts opts;
-	void *data;
-	LRet (*lfree) (List * list);
-	LIndex (*length) (List * list);
-	void *(*insert) (List * list, LIndex pos, void *data);
-	LRet (*remove) (List * list, LIndex pos, void *data);
-	void *(*get) (List * list, LIndex pos);
-	void *(*last) (List * list);
-	void *(*pushback) (List * list, void *data);
-        LRet (*popback) (List * list, void *data);
-	void *(*first) (List * list);
-	void *(*pushfront) (List * list, void *data);
-	LRet (*popfront) (List * list, void *data);
-	void *(*iter) (List * list, LIter * iter);
-	LRet (*clear) (List * list);
+    enum list_type type;
+    LOpts opts;
+    void *data;
+    LRet (*lfree) (List * list);
+    LIndex (*length) (List * list);
+    void *(*insert) (List * list, LIndex pos, void *data);
+    LRet (*remove) (List * list, LIndex pos, void *data);
+    void *(*get) (List * list, LIndex pos);
+    void *(*last) (List * list);
+    void *(*pushback) (List * list, void *data);
+    LRet (*popback) (List * list, void *data);
+    void *(*first) (List * list);
+    void *(*pushfront) (List * list, void *data);
+    LRet (*popfront) (List * list, void *data);
+    void *(*iter) (List * list, LIter * iter);
+    LRet (*clear) (List * list);
 
-	int (*equal) (void *data1, void *data2);
-	int (*cmp) (void *data1, void *data2);
-	LRet (*removefirst) (List * list, void *del, void *data);
-	LRet (*removelast) (List * list, void *del, void *data);
-//  LRet (*removeall)(List *list, void *del); /* maybe dangerous */
+    int (*equal) (void *data1, void *data2);
+    int (*cmp) (void *data1, void *data2);
+    LRet (*removefirst) (List * list, void *del, void *data);
+    LRet (*removelast) (List * list, void *del, void *data);
+    //  LRet (*removeall)(List *list, void *del); /* maybe dangerous */
 };
 
 #include "utils/list/list_dlinked.h"
 #include "utils/list/list_stlinked.h"
 #include "utils/list/list_array.h"
 
-#define L_NEW(_type, _opts)			(\
-                                        	  ((_type) == LIST_TYPE_DLINKED) ? (list_dlinked_new(_opts)) : (\
-						  ((_type) == LIST_TYPE_STLINKED) ? (list_stlinked_new(_opts)) : (\
-                                        	  ((_type) == LIST_TYPE_ARRAY) ? (list_array_new(_opts)) : (\
-						  NULL\
-						  )))\
-						)
+#define L_NEW(_type, _opts)			 (\
+                                         ((_type) == LIST_TYPE_DLINKED) ? (list_dlinked_new(_opts)) : (\
+                                         ((_type) == LIST_TYPE_STLINKED) ? (list_stlinked_new(_opts)) : (\
+                                         ((_type) == LIST_TYPE_ARRAY) ? (list_array_new(_opts)) : (\
+                                         NULL\
+                                         )))\
+                                     )
 
-#define L_SIZE(_list)				((_list) ? ((_list)->opts.size) : (0))
+#define L_SIZE(_list)                ((_list) ? ((_list)->opts.size) : (0))
 
-#define L_FREE(_list)				(((_list) && (_list)->lfree) ? ((_list)->lfree(_list)) : (LIST_ERR))
-#define L_LENGTH(_list)				(((_list) && (_list)->length) ? ((_list)->length(_list)) : (LIST_ERR))
-#define L_INSERT(_list, _pos, _data)		(((_list) && (_list)->insert) ? ((_list)->insert((_list), (_pos), (_data))) : (NULL))
-#define L_REMOVE(_list, _pos, _data)		(((_list) && (_list)->remove) ? ((_list)->remove((_list), (_pos), (_data))) : (LIST_ERR))
-#define L_GET(_list, _pos)			(((_list) && (_list)->get) ? ((_list)->get((_list), (_pos))) : (NULL))
+#define L_FREE(_list)                (((_list) && (_list)->lfree) ? ((_list)->lfree(_list)) : (LIST_ERR))
+#define L_LENGTH(_list)              (((_list) && (_list)->length) ? ((_list)->length(_list)) : (LIST_ERR))
+#define L_INSERT(_list, _pos, _data) (((_list) && (_list)->insert) ? ((_list)->insert((_list), (_pos), (_data))) : (NULL))
+#define L_REMOVE(_list, _pos, _data) (((_list) && (_list)->remove) ? ((_list)->remove((_list), (_pos), (_data))) : (LIST_ERR))
+#define L_GET(_list, _pos)			 (((_list) && (_list)->get) ? ((_list)->get((_list), (_pos))) : (NULL))
 
-#define L_LAST(_list)				(((_list) && (_list)->last) ? ((_list)->last((_list))) : (NULL))
-#define L_PUSHBACK(_list, _data)		(((_list) && (_list)->pushback) ? ((_list)->pushback((_list), (_data))) : (NULL))
-#define L_POPBACK(_list, _data)			(((_list) && (_list)->popback) ? ((_list)->popback((_list), (_data))) : (LIST_ERR))
+#define L_LAST(_list)                (((_list) && (_list)->last) ? ((_list)->last((_list))) : (NULL))
+#define L_PUSHBACK(_list, _data)     (((_list) && (_list)->pushback) ? ((_list)->pushback((_list), (_data))) : (NULL))
+#define L_POPBACK(_list, _data)      (((_list) && (_list)->popback) ? ((_list)->popback((_list), (_data))) : (LIST_ERR))
 
-#define L_FIRST(_list) 				(((_list) && (_list)->first) ? ((_list)->first(_list)) : (NULL))
-#define L_PUSHFRONT(_list, _data)		(((_list) && (_list)->pushfront) ? ((_list)->pushfront((_list), (_data))) : (NULL))
-#define L_POPFRONT(_list, _data)		(((_list) && (_list)->popfront) ? ((_list)->popfront((_list), (_data))) : (LIST_ERR))
+#define L_FIRST(_list)               (((_list) && (_list)->first) ? ((_list)->first(_list)) : (NULL))
+#define L_PUSHFRONT(_list, _data)    (((_list) && (_list)->pushfront) ? ((_list)->pushfront((_list), (_data))) : (NULL))
+#define L_POPFRONT(_list, _data)     (((_list) && (_list)->popfront) ? ((_list)->popfront((_list), (_data))) : (LIST_ERR))
 
 #define L_FOREACH(_list, _iter, _item)		if( !(_list) || !(_list)->iter ) \
-                                        	   INFO("Iterators not implementated.");\
-						else \
-                                        	for( (_item) = (_list)->iter(_list, _iter); \
-						     (_item) || (((_iter)->lfree) && (_iter)->lfree(_iter)); \
-						     (_item) = (_iter)->next(_iter) )
+                                                INFO("Iterators not implementated.");\
+                                            else \
+                                            for( (_item) = (_list)->iter(_list, _iter); \
+                                                 (_item) || (((_iter)->lfree) && (_iter)->lfree(_iter)); \
+                                                 (_item) = (_iter)->next(_iter) )
 
-#define L_ITERFREE(_iter)			(((_iter) && ((_iter)->lfree) ) ? ((_iter)->lfree(_iter)) : (LIST_ERR))
+#define L_ITERFREE(_iter)            (((_iter) && ((_iter)->lfree) ) ? ((_iter)->lfree(_iter)) : (LIST_ERR))
 
-#define L_CLEAR(_list)				(((_list) && (_list)->clear) ? ((_list)->clear(_list)) : (LIST_ERR))
+#define L_CLEAR(_list)               (((_list) && (_list)->clear) ? ((_list)->clear(_list)) : (LIST_ERR))
 
-#define L_SETEQUAL(_list, _equal)		(((_list) && (((_list)->equal = _equal) || 1)) ? (LIST_OK) : (LIST_ERR))
-#define L_REMOVEFIRST(_list, _del, _data)	(((_list) && (_list)->removefirst) ? ((_list)->removefirst(_list, _del, _data)) : (LIST_ERR))
-#define L_REMOVELAST(_list, _del, _data)	(((_list) && (_list)->removelast) ? ((_list)->removelast(_list, _del, _data)) : (LIST_ERR))
+#define L_SETEQUAL(_list, _equal)    (((_list) && (((_list)->equal = _equal) || 1)) ? (LIST_OK) : (LIST_ERR))
+#define L_REMOVEFIRST(_list, _del, _data)   (((_list) && (_list)->removefirst) ? ((_list)->removefirst(_list, _del, _data)) : (LIST_ERR))
+#define L_REMOVELAST(_list, _del, _data)    (((_list) && (_list)->removelast) ? ((_list)->removelast(_list, _del, _data)) : (LIST_ERR))
 //#define L_REMOVEALL(_list, _del)              (((_list) && (_list)->removeall) ? ((_list)->removeall(_list, _del)) : (LIST_ERR))
 
 #endif				/* __list_h__ */
